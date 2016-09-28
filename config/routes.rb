2b400464af53c99c838d7c928998e4583ebde07b
agdavid <antonio.g.david@gmail.com>
2016-09-28 15:53:47 -0400

    check links and remove extraneous routes, views

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root "welcome#home"
-
   get "/answers", to: "welcome#answers"
-  get "/contact", to: "welcome#contact"
 end
