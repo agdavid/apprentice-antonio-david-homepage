e081f96c810a500a6d414b34f3a95b59c1131494
agdavid <antonio.g.david@gmail.com>
2016-10-07 15:47:02 -0400

    add links to Edit and Delete Answer; add question Class Method to read author of answer

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,8 +1,13 @@
 class Question < ApplicationRecord
-  belongs_to :user
-  has_one :answer
+    belongs_to :user
+    has_one :answer
 
-  def printed_answer
-    self.answer.content
-  end
+    def printed_answer
+        self.answer.content
+    end
+
+    def answer_author
+        self.answer.user
+    end
+
 end
