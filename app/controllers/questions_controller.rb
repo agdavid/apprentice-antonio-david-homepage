050dd1e25a69a95d1c709c50234e149b7a4866e0
agdavid <antonio.g.david@gmail.com>
2016-10-07 13:45:23 -0400

    add CRUD routes and  actions to QuestionsController and AnswersController

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,8 +1,26 @@
 class QuestionsController < ApplicationController
 
-  def answers
-      #will automatically render views/questions/answers.html.erb
-      @questions = Question.all 
-  end
+    def answers
+        #will automatically render views/questions/answers.html.erb
+        @questions = Question.all 
+    end
 
+    def index
+    end
+
+    def new
+    end
+
+    def create
+    end
+
+    def show
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root "welcome#home"
-  get "/answers", to: "questions#answers"
   get "/map", to: "coordinates#map"
+
+  resources :questions
+  get "/answers", to: "questions#answers"
+  
+  resources :answers
   resources :coordinates, only: [:index]
+
 end
